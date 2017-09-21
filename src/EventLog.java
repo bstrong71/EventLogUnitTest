d74fcfde33b82f51951b407b1ce0fb70e4b1f4bf
Bernard Strong <bstrong71@gmail.com>
2017-09-21 15:12:37 -0400

    add classes and test folder

--- src/EventLog.java
+++ src/EventLog.java
@@ -1,2 +1,18 @@
+import java.util.List;
+
 public class EventLog {
+
+    private List<Event> eventList;
+
+    public boolean addEvent(Event event) {
+
+    }
+
+    public int getNumEvents() {
+
+    }
+
+    public EventLog() {
+
+    }
 }
